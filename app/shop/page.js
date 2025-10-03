858795437c61822303cc92661a20fd1b8ddedb58
Villymeta <Villyactive@gmail.com>
2025-10-03 01:06:10 -0700

    Update page.js

--- app/shop/page.js
+++ app/shop/page.js
@@ -15,7 +15,7 @@ export default function Page() {
   const [showShop, setShowShop] = useState(false); // NEW FLAG
 
   // ⏰ Countdown target date
-  const targetDate = new Date("2025-10-03T10:00:00");
+  const targetDate = new Date("2025-10-04T10:00:00");
 
   useEffect(() => {
     const interval = setInterval(() => {
